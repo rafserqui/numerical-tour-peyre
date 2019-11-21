4ac57efd57763c709881b4f87ae32d880ce0ef58
rafserqui <rafserqui@gmail.com>
2019-11-21 20:32:13 +0100

    Updated code on image gradients

--- optimization/gradient_divergence_images.m
+++ optimization/gradient_divergence_images.m
@@ -15,48 +15,26 @@ grad = @(x)cat(3,x - x([end 1:end-1],:), x-x(:,[end 1:end-1]));
 
 v = grad(x0);
 
-[FX,FY] = gradient(x0);
+N = 16;
+F = 1:N;
+F = reshape(F,[sqrt(N) sqrt(N)])';
+ddx = F([end 1:end-1],:);
+ddy = F(:,[end 1:end-1]);
 
 % figure('units','normalized','outerposition',[0 0 0.95 0.95])
-% subplot(2,2,1)
+% subplot(1,2,1)
 % imagesc(v(:,:,1))
+% axis square;
 % axis equal;
+% axis image;
+% colormap(gray)
 % title('d/dx')
 % 
-% subplot(2,2,3)
+% subplot(1,2,2)
 % imagesc(v(:,:,2))
+% axis square;
 % axis equal;
-% title('d/dy')
-% 
-% subplot(2,2,2)
-% imagesc(FX)
-% axis equal;
-% title('d/dx')
-% 
-% subplot(2,2,4)
-% imagesc(FY)
-% axis equal;
+% axis image;
+% colormap(gray)
 % title('d/dy')
 % export_fig(sprintf('%sgradient_descent_image',figs_folder),'-pdf','-transparent');
-
-dx = abs(FX - v(:,:,1));
-dx = abs(FY - v(:,:,2));
-
-[Gx,Gy] = imgradientxy(x0);
-
-figure
-subplot(2,2,1)
-imagesc(Gx)
-title('dx')
-
-subplot(2,2,2)
-imagesc(v(:,:,1))
-title('dx')
-
-subplot(2,2,3)
-imagesc(Gy)
-title('dy')
-
-subplot(2,2,4)
-imagesc(v(:,:,2))
-title('dy')
